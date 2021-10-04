542e28596b27a3ee390a20e399bd1a000325fc04
Castle <castlerock88@protonmail.com>
2021-10-04 13:05:12 -0400

    tougher than it should have been

--- convertStringCamelCase/index.js
+++ convertStringCamelCase/index.js
@@ -7,7 +7,17 @@
 
 
 function toCamelCase(str){
-
+	const words = str.split(/[_-]/g)
+	const answer = words.filter(x=>{
+		if(x!==words[0]){
+			return x
+		}
+	}).map(x=>{
+		return x.replace(x.charAt(0), x.charAt(0).toUpperCase())
+	})
+	const first = words.shift()
+	answer.unshift(first)
+	return answer.join('')
 }
 
 
@@ -15,3 +25,27 @@ console.log(toCamelCase('')) 				//''
 console.log(toCamelCase("the_stealth_warrior")) 	//"theStealthWarrior"
 console.log(toCamelCase("The-Stealth-Warrior")) 	//"TheStealthWarrior"
 console.log(toCamelCase("A-B-C"))			//"ABC"
+
+
+
+// const arr = str.split(/[_-]/)
+// 	const upper = arr.filter(x=>{
+// 		if(x !== arr[0]){
+// 			return x
+// 		}
+// 	})
+	
+// 	const first = arr.shift()
+// 	//camel.unshift(first)
+// 	//return upper.join('')
+// 	return upper
+
+// const answer = str.split(/[_-]/g)
+// 	const camel = answer.filter(x=>{
+// 		if(x === answer[0]){
+// 			x.charAt(0).toLowerCase() + x.substring(1)
+// 		}else{
+// 			x.charAt(0).toUpperCase() + x.substring(1)
+// 		}
+// 	})
+// 	return camel
