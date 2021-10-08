7d3b72f077abf66b61565d2714333414a3711f32
Castle <castlerock88@protonmail.com>
2021-10-08 05:33:35 -0400

    twotoone complete

--- twotoone/main.js
+++ twotoone/main.js
@@ -1,7 +1,12 @@
 // Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
 
 function longest(s1, s2) {
-  // your code
+  let answer = []
+  let stringCombo = s1.concat(s2)
+  let stringArr = stringCombo.split('').forEach(x=>{
+	if(!answer.includes(x)) answer.push(x)
+  })
+  return answer.sort().join('')
 }
 
 
