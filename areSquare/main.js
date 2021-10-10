8accaf1e19dfa6256174799de39a78c1f44b4618
Castle <castlerock88@protonmail.com>
2021-10-09 23:56:51 -0400

    are elements in array square?

---
+++ areSquare/main.js
@@ -0,0 +1,17 @@
+// Write a function that checks whether all elements in an array are square numbers. The function should be able to take any number of array elements.
+
+// Your function should return true if all elements in the array are square numbers and false if not.
+
+// An empty array should return undefined / None / nil. You can assume that all array elements will be positive integers.
+
+
+var isSquare = function(arr){
+  
+}
+
+
+
+console.log(isSquare([1, 4, 9, 16, 25, 36])) 			//true);
+console.log(isSquare([1, 2, 3, 4, 5, 6])) 			//false);
+console.log(isSquare([])) 					//undefined);
+console.log(isSquare([1, 2, 4, 15])) 				//false); 
