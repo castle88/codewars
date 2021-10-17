1db7b8ddd7a133b542d114da98f8a938e5028785
Castle <castlerock88@protonmail.com>
2021-10-17 01:50:49 -0400

    regex version attempt

--- printerError/main.js
+++ printerError/main.js
@@ -13,8 +13,9 @@ function printerError(s) {
     let errRegex = /[n-z]/gm
     let errors = [ 'n','o','p','q','r','s','t','u','v','w','x','y','z' ]
     let answer = []
-    let strArr = s.split('').forEach(x=>errors.includes(x)?answer.push(x):console.log('no'))
-    return answer
+    // let strArr = s.split('').forEach(x=>errors.includes(x)?answer.push(x):console.log('no'))
+    let strArr = s.split('').forEach(x => x.match(errRegex)? answer.push(x) : console.log('no'))
+    return answer.length
 }
 
 
