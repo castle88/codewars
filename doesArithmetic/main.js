e1802aeeab3c571210719802af33e1c6feedba2a
Castle <castlerock88@protonmail.com>
2021-10-21 05:19:38 -0400

    ezpz

--- doesArithmetic/main.js
+++ doesArithmetic/main.js
@@ -16,7 +16,20 @@
 
 
 function arithmetic(a, b, operator){
-  //your code here!
+  switch(operator){
+	case 'add':
+		return a + b;
+		break;
+	case 'subtract':
+		return a - b;
+		break;
+	case 'multiply':
+		return a * b;
+		break;
+	case 'divide':
+		return a / b;
+		break;
+  }
 }
 
 
