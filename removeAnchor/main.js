28425d5f33e9e7ee32b61bc82b4ec486d554806f
Castle <castlerock88@protonmail.com>
2021-10-23 03:26:12 -0400

    removeAnchor setup

---
+++ removeAnchor/main.js
@@ -0,0 +1,12 @@
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed. 
+
+
+function removeUrlAnchor(url){
+	
+}
+
+
+
+console.log(removeUrlAnchor('www.codewars.com#about'))			// 'www.codewars.com'
+console.log(removeUrlAnchor('www.codewars.com/katas/?page=1#about'))	// 'www.codewars.com/katas/?page=1'
+console.log(removeUrlAnchor('www.codewars.com/katas/'))			// 'www.codewars.com/katas/'
