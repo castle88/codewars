2c244f43f25b01683f72b238eeeb91d719e7cbb4
Castle <castlerock88@protonmail.com>
2021-10-24 08:01:07 -0400

    start camel case break

---
+++ breakCamelCase/main.js
@@ -0,0 +1,10 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+
+function solution(string) {
+    
+}
+
+
+console.log(solution('camelCasing'))		// 'camel Casing'
+console.log(solution('camelCasingTest'))	// 'camel Casing Test'
