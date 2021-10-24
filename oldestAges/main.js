89fb0fe7795b63315e8bd5984237268ce34e6ba5
Castle <castlerock88@protonmail.com>
2021-10-24 07:50:42 -0400

    sorted behaives as expected

--- oldestAges/main.js
+++ oldestAges/main.js
@@ -3,7 +3,8 @@
 // The order of the numbers passed in could be any order. The array will always include at least 2 items. If there are two or more oldest age, then return both of them in array format.
 
 function twoOldestAges(ages){
-
+	let sorted = ages.sort((a,b)=>a-b)
+	return sorted
 }
 
 
