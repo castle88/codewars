fe1e4e5456702fee81163e0b0cd7d57aaaf51f67
Castle <castlerock88@protonmail.com>
2021-10-26 10:43:02 -0400

    twice as old setup

---
+++ twiceAsOld/main.js
@@ -0,0 +1,17 @@
+// Your function takes two arguments:
+
+//     current father's age (years)
+//     current age of his son (years)
+
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+	
+}
+
+console.log(twiceAsOld(36,7)) 	// 22
+console.log(twiceAsOld(55,30))  // 5
+console.log(twiceAsOld(42,21)) 	// 0
+console.log(twiceAsOld(22,1)) 	// 20
+console.log(twiceAsOld(29,0)) 	// 29
