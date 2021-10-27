eefd428fb2beebaf77347350793e208d330df643
Castle <castlerock88@protonmail.com>
2021-10-27 08:18:46 -0400

    added incrementor

--- unfinishedLoop/main.js
+++ unfinishedLoop/main.js
@@ -3,7 +3,7 @@
 function createArray(number){
   var newArray = [];
   
-  for(var counter = 1; counter <= number;){
+  for(var counter = 1; counter <= number; counter++){
     newArray.push(counter);
   }
   
