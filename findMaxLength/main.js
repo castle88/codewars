59281b2b73eb3f8aa5d19a3c30779c12ed10657f
Castle <castlerock88@protonmail.com>
2021-10-27 08:28:24 -0400

    find max length difference setup

---
+++ findMaxLength/main.js
@@ -0,0 +1,14 @@
+// You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z. Let x be any string in the first array and y be any string in the second array.
+
+// Find max(abs(length(x) − length(y)))
+
+// If a1 and/or a2 are empty return -1 in each language except in Haskell (F#) where you will return Nothing (None).
+
+function mxdiflg(a1, a2) {
+    return a1.join(' ').length - a2.join(' ').length
+}
+
+
+
+
+console.log(mxdiflg(["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"], ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]))	// 13
