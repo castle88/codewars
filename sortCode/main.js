f4949276030729b942ec576d380cb660a8fd43d2
Castle <castlerock88@protonmail.com>
2021-10-28 10:51:28 -0400

    sorted code

--- sortCode/main.js
+++ sortCode/main.js
@@ -8,7 +8,7 @@
 // Write a function called sortGiftCode/sort_gift_code/SortGiftCode that accepts a string containing up to 26 unique alphabetical characters, and returns a string containing the same characters in alphabetical order.
 
 function sortGiftCode(code){
-	return code.split('').sort((a,b)=>a-b).join('')
+	return code.split('').sort().join('')
 }
 
 console.log(sortGiftCode('abcdef'))				// 'abcdef'
