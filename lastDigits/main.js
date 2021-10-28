a6e5c29e2ede3a93d10684d8d2c82e2210a65050
Castle <castlerock88@protonmail.com>
2021-10-28 10:54:24 -0400

    lastDigits setup

---
+++ lastDigits/main.js
@@ -0,0 +1,15 @@
+// Your job is to implement a function which returns the last D digits of an integer N as a list.
+// Special cases:
+
+//     If D > (the number of digits of N), return all the digits.
+//     If D <= 0, return an empty list.
+
+function lastDigit(n, d) {
+  
+}
+
+
+console.log(lastDigit(1,1))			// [1])
+console.log(lastDigit(123767,4))	 	// [3,7,6,7])
+console.log(lastDigit(0,1))			// [0])
+console.log(lastDigit(34625647867585,10))	// [5,6,4,7,8,6,7,5,8,5])
