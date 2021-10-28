395bf1741ae1ec14ea38e03bd56dba63b48d84ee
Castle <castlerock88@protonmail.com>
2021-10-28 10:23:53 -0400

    move exclamation setup

---
+++ moveExclamation/main.js
@@ -0,0 +1,23 @@
+// Description:
+
+// Move all exclamation marks to the end of the sentence
+// Examples
+
+// remove("Hi!") === "Hi!"
+// remove("Hi! Hi!") === "Hi Hi!!"
+// remove("Hi! Hi! Hi!") === "Hi Hi Hi!!!"
+// remove("Hi! !Hi Hi!") === "Hi Hi Hi!!!"
+// remove("Hi! Hi!! Hi!") === "Hi Hi Hi!!!!"
+
+function remove(s){
+  //coding and coding....
+  
+  
+}
+
+
+console.log(remove("Hi!"))			// "Hi!")
+console.log(remove("Hi! Hi!"))			// "Hi Hi!!")
+console.log(remove("Hi! Hi! Hi!"))		// "Hi Hi Hi!!!")
+console.log(remove("Hi! !Hi Hi!"))		// "Hi Hi Hi!!!")
+console.log(remove("Hi! Hi!! Hi!"))		// "Hi Hi Hi!!!!")
