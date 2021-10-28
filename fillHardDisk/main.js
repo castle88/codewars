4214feb7bf667d45016a4d971aa7f1dddf04ee33
Castle <castlerock88@protonmail.com>
2021-10-28 09:58:58 -0400

    fill hard drive setup

---
+++ fillHardDisk/main.js
@@ -0,0 +1,23 @@
+// Your task is to determine how many files of the copy queue you will be able to save into your Hard Disk Drive. The files must be saved in the order they appear in the queue.
+// Input:
+
+//     Array of file sizes (0 <= s <= 100)
+//     Capacity of the HD (0 <= c <= 500)
+
+// Output:
+
+//     Number of files that can be fully saved in the HD.
+
+function save(sizes, hd) {
+  // your code here
+}
+
+
+save([4, 4, 4, 3, 3])		// 12
+save([4, 4, 4, 3, 3])		// 11
+save([4, 8, 15, 16, 23, 42])	// 108
+save([13])			// 13
+save([1, 2, 3, 4])		// 250
+save([100])			// 500
+save([11, 13, 15, 17, 19])	// 8
+save([45])			// 12
