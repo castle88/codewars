5d91e204750389a4fa5744e097a03847accf3168
Castle <castlerock88@protonmail.com>
2021-10-28 10:33:01 -0400

    first attempt exclamation

--- moveExclamation/main.js
+++ moveExclamation/main.js
@@ -11,13 +11,14 @@
 
 function remove(s){
 	let split = s.split('')
-	let filtered = split.filter(x => x !== '!')
-	return filtered
+	let filtered = split.filter(x => x !== '!').join('')
+	let points = split.filter(x => x === '!').join('')
+	return filtered.concat(points)
 }
 
 
-console.log(remove("Hi!"))			// "Hi!")
-console.log(remove("Hi! Hi!"))			// "Hi Hi!!")
-console.log(remove("Hi! Hi! Hi!"))		// "Hi Hi Hi!!!")
-console.log(remove("Hi! !Hi Hi!"))		// "Hi Hi Hi!!!")
-console.log(remove("Hi! Hi!! Hi!"))		// "Hi Hi Hi!!!!")
+console.log(remove("Hi!"))			// "Hi!"
+console.log(remove("Hi! Hi!"))			// "Hi Hi!!"
+console.log(remove("Hi! Hi! Hi!"))		// "Hi Hi Hi!!!"
+console.log(remove("Hi! !Hi Hi!"))		// "Hi Hi Hi!!!"
+console.log(remove("Hi! Hi!! Hi!"))		// "Hi Hi Hi!!!!"
