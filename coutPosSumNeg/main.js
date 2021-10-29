4c82359a1413b426d59d359efb7f5de9f6d16ecd
Castle <castlerock88@protonmail.com>
2021-10-29 12:33:35 -0400

    count pos sum neg setup and testing careful spelled directory wrong

---
+++ coutPosSumNeg/main.js
@@ -0,0 +1,19 @@
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.
+
+// If the input array is empty or null, return an empty array.
+// Example
+
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+
+function countPositivesSumNegatives(input) {
+    let count = input.filter(x => x > 0).length
+    let sum = input.filter(x => x < 0).reduce((acc, cur) => acc += cur)
+    return [count, sum]
+}
+
+
+
+console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))	// [10, -65].
