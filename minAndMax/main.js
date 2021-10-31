9a402988e92a8231abd3266afa9d7fe999bebe67
Castle <castlerock88@protonmail.com>
2021-10-31 16:33:25 -0400

    typo in solutions

--- minAndMax/main.js
+++ minAndMax/main.js
@@ -21,7 +21,7 @@ var max = function(list){
 }
 
 
-console.log(min([-52, 56, 30, 29, -54, 0, -110])) 	// 110
+console.log(min([-52, 56, 30, 29, -54, 0, -110])) 	// -110
 console.log(min([42, 54, 65, 87, 0])) 			// 0
 console.log(max([4,6,2,1,9,63,-134,566])) 		// 566
 console.log(max([5])) 					// 5
