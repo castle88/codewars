54e2a963ef1ba0e62c35b2b9d7cbde26a6524a99
Castle <castlerock88@protonmail.com>
2021-10-31 17:47:14 -0400

    less than or equal to

--- fizzBuzz/main.js
+++ fizzBuzz/main.js
@@ -15,7 +15,7 @@
 
 function fizzbuzz(n){
 	let answer = []
-	for(let i = 1; i < n; i++){
+	for(let i = 1; i <= n; i++){
 		(i % 3 === 0 && i % 5 === 0) ? answer.push('FizzBuzz')
 		:(i % 3 === 0) ? answer.push('Fizz')
 		:(i % 5 === 0) ? answer.push('Buzz')
