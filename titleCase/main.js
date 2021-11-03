781e8e9520f4272c093d569eb2bd369f92d33856
Castle <castlerock88@protonmail.com>
2021-11-02 22:50:51 -0400

    more setup

--- titleCase/main.js
+++ titleCase/main.js
@@ -21,3 +21,9 @@
 function titleCase(title, minorWords) {
   
 }
+
+
+console.log(titleCase(''))						// ''
+console.log(titleCase('a clash of KINGS', 'a an the of'))		// 'A Clash of Kings'
+console.log(titleCase('THE WIND IN THE WILLOWS', 'The In'))		// 'The Wind in the Willows'
+console.log(titleCase('the quick brown fox'))		 		// 'The Quick Brown Fox'
