671594963feead859bd08f2221647c3149735d21
Castle <castlerock88@protonmail.com>
2021-11-02 21:15:17 -0400

    repeater setup

---
+++ repeater/main.js
@@ -0,0 +1,14 @@
+// Write a function named repeater() that takes two arguments (a string and a number), and returns a new string where the input string is repeated that many times.
+// Example: (Input1, Input2 --> Output)
+
+// "a", 5 --> "aaaaa"
+
+
+
+function repeater(string, n){
+  //Your code goes here.
+}
+
+assert.strictEqual(repeater('a', 5))		// 'aaaaa'
+assert.strictEqual(repeater('Na', 16))		// 'NaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNa'
+assert.strictEqual(repeater('Wub ', 6))		// 'Wub Wub Wub Wub Wub Wub '
