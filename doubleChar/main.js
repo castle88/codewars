69b4ae1a6cb577bce4b75c821f10b322821a31a6
Castle <castlerock88@protonmail.com>
2021-11-04 04:27:35 -0400

    double char setup

---
+++ doubleChar/main.js
@@ -0,0 +1,22 @@
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+// doubleChar("String") ==> "SSttrriinngg"
+
+// doubleChar("Hello World") ==> "HHeelllloo  WWoorrlldd"
+
+// doubleChar("1234!_ ") ==> "11223344!!__  "
+
+// Good Luck!
+
+
+function doubleChar(str) {
+  // Your code here
+}
+
+
+console.log(doubleChar("abcd"))				// "aabbccdd"
+console.log(doubleChar("Adidas"))			// "AAddiiddaass"
+console.log(doubleChar("1337"))				// "11333377"
+console.log(doubleChar("illuminati"))			// "iilllluummiinnaattii"
+console.log(doubleChar("123456"))			// "112233445566"
+console.log(doubleChar("%^&*("))			// "%%^^&&**(("
