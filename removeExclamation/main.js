d63b7995d92951633398d9d8685a5ba3fc0e82d5
Castle <castlerock88@protonmail.com>
2021-11-05 04:43:19 -0400

    remove exclamations

---
+++ removeExclamation/main.js
@@ -0,0 +1,18 @@
+// Description:
+
+// Remove all exclamation marks from sentence except at the end.
+
+
+function remove(s){
+  //coding and coding....
+  
+  
+}
+
+
+console.log(remove("Hi!"))		//  "Hi!"
+console.log(remove("Hi!!!"))		//  "Hi!!!"
+console.log(remove("!Hi"))		//  "Hi"
+console.log(remove("!Hi!"))		//  "Hi!"
+console.log(remove("Hi! Hi!"))		//  "Hi Hi!"
+console.log(remove("Hi"))		//  "Hi"
