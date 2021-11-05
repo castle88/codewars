5ffe2bb864b08b4f80d61549d51aed008c10ffbd
Castle <castlerock88@protonmail.com>
2021-11-05 05:49:48 -0400

    lots of fails

--- warriorKyu/main.js
+++ warriorKyu/main.js
@@ -18,7 +18,80 @@
 // ([1,7,2,3,4,5,9,10], 7) => 4
 
 function getKyu(allHonors, honor){
-
+	console.log(`${allHonors} -- ${honor}`)
+	let sorted = allHonors.sort((a,b) => a-b)
+	let sliced = sorted.filter(x => x <= honor)
+	switch(sliced.indexOf(honor)+1){
+		case 1:
+			return 1
+			break;
+		case 2:
+			return 2
+			break;
+		case 3: 
+			return 2
+			break;
+		case 4:
+			return 3
+			break;
+		case 5:
+			return 3
+			break;
+		case 6:
+			return 4
+			break;
+		case 7:
+			return 4
+			break;
+		case 8:
+			return 4
+			break;
+		case 9: 
+			return 5
+			break;
+		case 10:
+			return 5
+			break;
+		case 11:
+			return 5
+			break;
+		case 12:
+			return 6
+			break;
+		case 13:
+			return 6
+			break;
+		case 14:
+			return 6
+			break;
+		case 15:
+			return 4
+			break;
+		case 16:
+			return 4
+			break;
+		case 17:
+			return 4
+			break;
+		case 18:
+			return 4
+			break;
+		case 19:
+			return 5
+			break;
+		case 20:
+			return 5
+			break;
+		case 21:
+			return 5
+			break;
+		case 22:
+			return 5
+			break;
+		default:
+			return 0
+			break;		
+	}
 }
 
 console.log(getKyu([66,12,90,15], 15))			// 2 		
