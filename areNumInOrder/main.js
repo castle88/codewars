8886271703a64503510d27e967735f5008c052bb
Castle <castlerock88@protonmail.com>
2021-11-06 04:33:41 -0400

    failed kata

--- areNumInOrder/main.js
+++ areNumInOrder/main.js
@@ -17,11 +17,13 @@
 
 
 function inAscOrder(arr) {
-
+	let sortMe = arr.map(x => x)
+	sortMe.sort((a, b) => a - b)
+	return sortMe.every((x, i) => x === arr[i])
 }
 
 
 console.log(inAscOrder([1, 2, 4, 7, 19]))				// 'The list of numbers "1, 2, 4, 7, 19" is in ascending order'
 console.log(inAscOrder([1, 2, 3, 4, 5]))				// 'The list of numbers "1, 2, 3, 4, 5" is in ascending order'
-console.log(!inAscOrder([1, 6, 10, 18, 2, 4, 20]))			// 'The list of numbers "1, 6, 10, 18, 2, 4, 20" is not in ascending order'
-console.log(!inAscOrder([9, 8, 7, 6, 5, 4, 3, 2, 1]))			// 'The list of numbers "9, 8, 7, 6, 5, 4, 3, 2, 1" is in DESCENDING order not ascending order'
+console.log(inAscOrder([1, 6, 10, 18, 2, 4, 20]))			// 'The list of numbers "1, 6, 10, 18, 2, 4, 20" is not in ascending order'
+console.log(inAscOrder([9, 8, 7, 6, 5, 4, 3, 2, 1]))			// 'The list of numbers "9, 8, 7, 6, 5, 4, 3, 2, 1" is in DESCENDING order not ascending order'
