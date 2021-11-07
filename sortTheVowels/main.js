0e315f8482cc4242b6dcb61fc6b4cedecb24eca5
Castle <castlerock88@protonmail.com>
2021-11-07 01:27:09 -0400

    solved?

--- sortTheVowels/main.js
+++ sortTheVowels/main.js
@@ -24,10 +24,23 @@
 //     Invalid input ( undefined / null / integer ) should return an empty string
 
 function sortVowels(s){
-	
+	if(typeof s !== 'string') return ''
+	let arr = s.split('')
+	return arr.map((x, i) => {
+		if(i === arr.length - 1) return x.match(/[aeiou]/gm) ? `|${x}` : `${x}|`
+		if(x.match(/[aeiou]/gm)){
+			return `|${x}\n`
+		}else{
+			return `${x}|\n`
+		}
+	}).join('')
 }
 
 console.log(sortVowels('Codewars'))			// 'C|\n|o\nd|\n|e\nw|\n|a\nr|\ns|');
-console.log(sortVowels('Rnd Te5T'))			// 'R|\nn|\nd|\n |\nT|\n|e\n5|\nT|');
+console.log(sortVowels('Rnd Te5T'))			// 'R|\n  n|\n  d|\n |\n  T|\n  |e\n  5|\n  T|');
 console.log(sortVowels(1337))				// '');
 console.log(sortVowels(undefined))			// '');
+
+
+//  'C|\n  |o\n  d|\n  |e\n  w|\n  |a\n  r|\n  s|', 
+//: 'C|\n  |o\n  d|\n  |e\n  w|\n  |a\n  r|\n  s|\n  '
