fa8d8ac72dd711efe12668122fa41113fa1a54f6
Castle <castlerock88@protonmail.com>
2021-11-07 00:54:01 -0400

    wrong parameters

--- needleHaystack/main.js
+++ needleHaystack/main.js
@@ -12,10 +12,10 @@
 
 
 function findNeedle(haystack) {
-  // your code here
+	return haystack.indexOf('needle')
 }
 
 
-console.log(findNeedle(haystack_1))		// 'found the needle at position 3'
-console.log(findNeedle(haystack_2))		// 'found the needle at position 5'
-console.log(findNeedle(haystack_3))		// 'found the needle at position 30'
+console.log(findNeedle(['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false]))		// 'found the needle at position 3'
+console.log(findNeedle(['283497238987234', 'a dog', 'a cat', 'some random junk', 'a piece of hay', 'needle', 'something somebody lost a while ago']))		// 'found the needle at position 5'
+console.log(findNeedle([1,2,3,4,5,6,7,8,8,7,5,4,3,4,5,6,67,5,5,3,3,4,2,34,234,23,4,234,324,324,'needle',1,2,3,4,5,5,6,5,4,32,3,45,54]))		// 'found the needle at position 30'
