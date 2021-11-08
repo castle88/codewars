f412ba27339d771900c6863f47df2d35f13f150c
Castle <castlerock88@protonmail.com>
2021-11-08 03:35:16 -0500

    failed

--- countMonkeys/main.js
+++ countMonkeys/main.js
@@ -16,7 +16,6 @@ function monkeyCount(n) {
 	return answer
 }
 
-
 console.log((monkeyCount(5)))			// [1, 2, 3, 4, 5]
 console.log((monkeyCount(3)))			// [1, 2, 3]
 console.log((monkeyCount(9)))			// [1, 2, 3, 4, 5, 6, 7, 8, 9]
