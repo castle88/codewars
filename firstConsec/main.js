63d51c099f9a1e4e39144ab3f09e1f4077881665
Castle <castlerock88@protonmail.com>
2021-11-08 22:03:01 -0500

    find non consec test

--- firstConsec/main.js
+++ firstConsec/main.js
@@ -21,8 +21,10 @@
 
 
 function firstNonConsecutive (arr) {
-
+	for(let i = 0; i <= arr.length; i++){
+		if(arr[i] + 1 !== arr[i+1]) return arr[i]
+	}
 }
 
 
-console.log(firstNonConsecutive([1,2,3,4,6,7,8]))
+console.log(firstNonConsecutive([1,2,3,4,6,7,8]))	// 6
