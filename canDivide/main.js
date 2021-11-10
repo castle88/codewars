fb0a5741d45ead6f2e0bc55fd73b203b0c648f58
Castle <castlerock88@protonmail.com>
2021-11-09 23:03:24 -0500

    pretty easy

--- canDivide/main.js
+++ canDivide/main.js
@@ -13,7 +13,8 @@
 // (15, -5, 3)   ->  true
 
 function isDivideBy(number, a, b) {
-	
+	if(number % a === 0 && number % b === 0) return true
+	return false
 }
 
 console.log(isDivideBy(-12, 2, -6))		// true
