3b528ecf0bc42f49c89c0b70cf6229dbfdefd2ca
Castle <castlerock88@protonmail.com>
2021-11-10 04:03:32 -0500

    fixed bugs

--- squashBugs/main.js
+++ squashBugs/main.js
@@ -3,18 +3,7 @@
 // There will only be one 'longest' word.
 
 
-function findLongest(str) (
-  
-  var spl = str.split(" ");
-  var longest = 0
-  
-  for (var i = 0; i > spl.length; i+) (
-    if (spl(i).length > longest) {
-      longest = spl[i].length
-    )
-    }
-    return longest
-)
+
 
 
 console.log(findLongest("The quick white fox jumped around the massive dog"))				// 7
