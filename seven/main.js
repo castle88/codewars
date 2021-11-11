66e971ec7cf0433bb6844785cd9bf004e155cd93
Castle <castlerock88@protonmail.com>
2021-11-11 00:37:16 -0500

    seven setup

---
+++ seven/main.js
@@ -0,0 +1,14 @@
+// Write a function that removes every lone 9 that is inbetween 7s.
+
+// sevenAte9('79712312') => '7712312'
+// sevenAte9('79797') => '777'
+
+
+function sevenAte9(str){
+
+}
+
+
+console.log(sevenAte9('165561786121789797'))		// '16556178612178977'
+console.log(sevenAte9('797'))				// '77'
+console.log(sevenAte9('7979797'))			// '7777'
