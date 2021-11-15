a224eb46b8eda2dc6c7d1bb5ca89dd892593bdf2
Castle <castlerock88@protonmail.com>
2021-11-15 01:06:21 -0500

    tests wrong format

--- altCase/main.js
+++ altCase/main.js
@@ -13,16 +13,16 @@
 // As usual, your function/method should be pure, i.e. it should not mutate the original string.
 
 
-function toAlternatingCase() {
-  // Define your method here :)
+function toAlternatingCase(str) {
+
 }
 
 
-console.log("hello world".toAlternatingCase())					// "HELLO WORLD"
-console.log("HELLO WORLD".toAlternatingCase())					// "hello world"
-console.log("hello WORLD".toAlternatingCase())					// "HELLO world"
-console.log("HeLLo WoRLD".toAlternatingCase())					// "hEllO wOrld"
-console.log("12345".toAlternatingCase())					// "12345"
-console.log("1a2b3c4d5e".toAlternatingCase())					// "1A2B3C4D5E"
-console.log("String.prototype.toAlternatingCase".toAlternatingCase())		// "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
+console.log(toAlternatingCase("hello world"))					// "HELLO WORLD"
+console.log(toAlternatingCase("HELLO WORLD"))					// "hello world"
+console.log(toAlternatingCase("hello WORLD"))					// "HELLO world"
+console.log(toAlternatingCase("HeLLo WoRLD"))					// "hEllO wOrld"
+console.log(toAlternatingCase("12345"))						// "12345"
+console.log(toAlternatingCase("1a2b3c4d5e"))					// "1A2B3C4D5E"
+console.log(toAlternatingCase("String.prototype.toAlternatingCase"))		// "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
 // console.log("Hello World".toAlternatingCase().toAlternatingCase(), "Hello World"
