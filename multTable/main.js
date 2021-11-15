eae1cfb607962606d7c43f6ca6e2308359aa3a93
Castle <castlerock88@protonmail.com>
2021-11-15 01:53:04 -0500

    answer string wrong format

--- multTable/main.js
+++ multTable/main.js
@@ -23,9 +23,9 @@ function multiTable(number) {
 	for(let i = 1; i <= 10; i++){
 		let answer = number * i
 		if(i === 10){
-			arr.push(`${number} * ${i} = ${answer}`)
+			arr.push(`${i} * ${number} = ${answer}`)
 		}else{
-			arr.push(`${number} * ${i} = ${answer}\n`)
+			arr.push(`${i} * ${number} = ${answer}\n`)
 		}
 	}
 
@@ -34,3 +34,7 @@ function multiTable(number) {
 
 console.log(multiTable(5))	// '1 * 5 = 5\n2 * 5 = 10\n3 * 5 = 15\n4 * 5 = 20\n5 * 5 = 25\n6 * 5 = 30\n7 * 5 = 35\n8 * 5 = 40\n9 * 5 = 45\n10 * 5 = 50'
 console.log(multiTable(1))	// '1 * 1 = 1\n2 * 1 = 2\n3 * 1 = 3\n4 * 1 = 4\n5 * 1 = 5\n6 * 1 = 6\n7 * 1 = 7\n8 * 1 = 8\n9 * 1 = 9\n10 * 1 = 10'
+
+
+expected '5 * 1 = 5\n5 * 2 = 10\n5 * 3 = 15\n5 * 4 = 20\n5 * 5 = 25\n5 * 6 = 30\n5 * 7 = 35\n5 * 8 = 40\n5 * 9 = 45\n5 * 10 = 50' 
+to equal '1 * 5 = 5\n2 * 5 = 10\n3 * 5 = 15\n4 * 5 = 20\n5 * 5 = 25\n6 * 5 = 30\n7 * 5 = 35\n8 * 5 = 40\n9 * 5 = 45\n10 * 5 = 50'
