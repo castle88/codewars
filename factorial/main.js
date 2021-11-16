38b7a58731584a3de4b779e64988978fc89fca23
Castle <castlerock88@protonmail.com>
2021-11-16 05:24:30 -0500

    factorial setup

---
+++ factorial/main.js
@@ -0,0 +1,11 @@
+// Make a function factorial
+
+function factorial(n){
+  //your code here
+}
+
+console.log(factorial(0))		//  1
+console.log(factorial(1))		//  1
+console.log(factorial(4))		//  24
+console.log(factorial(7))		//  5040
+console.log(factorial(17))		//  355687428096000
