4f9a52fa412b745ce13581f04c4ce93372ad443f
Castle <castlerock88@protonmail.com>
2021-11-17 11:44:09 -0500

    old switcheroo setup

---
+++ oldSwitcheroo2/main.js
@@ -0,0 +1,26 @@
+// This is a follow up from my kata The old switcheroo</br/>
+
+// Write
+
+// function encode(str)
+
+// that takes in a string str and replaces all the letters with their respective positions in the English alphabet.
+
+// encode('abc') == '123'   // a is 1st in English alpabet, b is 2nd and c is 3rd
+// encode('codewars') == '315452311819'
+// encode('abc-#@5') == '123-#@5'
+
+// String are case sensitive.
+
+// // Bonus point if you don't use toLowerCase()
+
+
+
+function encode(str) {
+ //code me
+}
+
+console.log(encode('abc'))			// '123'
+console.log(encode('ABCD'))			// '1234'
+console.log(encode('ZzzzZ'))			// '2626262626'
+console.log(encode('abc-#@5'))			// '123-#@5'
