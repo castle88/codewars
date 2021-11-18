8b2d9b2c37aa9ef5a54bca65cc8e0a99644b048c
Castle <castlerock88@protonmail.com>
2021-11-18 09:05:35 -0500

    leap years

--- leapYears/main.js
+++ leapYears/main.js
@@ -12,7 +12,16 @@
 
 
 function isLeapYear(year) {
-	
+	if(year % 4 === 0){
+		if(year % 100 !== 0){
+			return true
+		}else{
+			if(year % 400 === 0){
+				return true
+			}
+		}
+	}
+	return false
 }
 
 
