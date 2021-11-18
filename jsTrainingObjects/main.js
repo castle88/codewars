b24818f1fad9fd255ea5173745c68a72e4f45158
Castle <castlerock88@protonmail.com>
2021-11-18 10:47:37 -0500

    testing

--- jsTrainingObjects/main.js
+++ jsTrainingObjects/main.js
@@ -24,7 +24,7 @@
 // In the end, click "Submit" to submit your code pass this kata.
 
 function animal(obj){
-  
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`
 }
 
 console.log(animal({name:"dog",legs:4,color:"white"}))			// "This white dog has 4 legs."
