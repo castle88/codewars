f593a8ace52a2bbe104ac03792a5bdba3b4fb2a8
Castle <castlerock88@protonmail.com>
2021-11-20 17:49:17 -0500

    possilbly solved

--- fccStuff/main.js
+++ fccStuff/main.js
@@ -1,11 +1,39 @@
-function urlSlug(title) {
-	return title.toLowerCase().split(' ').filter(x => x !== '').join('-')
+// function urlSlug(title) {
+// 	return title.toLowerCase().split(' ').filter(x => x !== '').join('-')
+// }
+
+// console.log(urlSlug("Winter Is Coming"))	// should return the string winter-is-coming.
+
+// console.log(urlSlug(" Winter Is  Coming"))	// should return the string winter-is-coming.
+
+// //console.log(urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone"))	// should return the string a-mind-needs-books-like-a-sword-needs-a-whetstone.
+
+// //console.log(urlSlug("Hold The Door"))	// should return the string hold-the-door.
+
+
+
+
+// ****************************************************************
+
+// palindrome checker
+
+function palindrome(str) {
+  let filtered = str.toLowerCase().split('').filter(x => x.match(/[a-z0-9]/))
+  return filtered.join('') === filtered.reverse().join('') ? true : false
 }
 
-console.log(urlSlug("Winter Is Coming"))	// should return the string winter-is-coming.
+console.log(palindrome("eye"))						//  true.
+console.log(palindrome("_eye"))						//  true.
+console.log(palindrome("race car"))					//  true.
 
-console.log(urlSlug(" Winter Is  Coming"))	// should return the string winter-is-coming.
+console.log(palindrome("not a palindrome"))				//  false.
+console.log(palindrome("A man, a plan, a canal. Panama"))		//  true.
+console.log(palindrome("never odd or even"))				//  true.
 
-//console.log(urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone"))	// should return the string a-mind-needs-books-like-a-sword-needs-a-whetstone.
+console.log(palindrome("nope"))						//  false.
+console.log(palindrome("almostomla"))					//  false.
+console.log(palindrome("My age is 0, 0 si ega ym."))			//  true.
 
-//console.log(urlSlug("Hold The Door"))	// should return the string hold-the-door.
+console.log(palindrome("1 eye for of 1 eye."))				//  false.
+console.log(palindrome("0_0 (: /-\ :) 0-0"))				//  true.
+console.log(palindrome("five|\_/|four"))				//  false.
