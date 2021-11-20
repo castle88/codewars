7ff556978aedb45d3ab97aa65f8082ebb3da0a36
Castle <castlerock88@protonmail.com>
2021-11-19 19:11:58 -0500

    testing

---
+++ loveFriendship/main.js
@@ -0,0 +1,23 @@
+// If　a = 1, b = 2, c = 3 ... z = 26
+
+// Then l + o + v + e = 54
+
+// and f + r + i + e + n + d + s + h + i + p = 108
+
+// So friendship is twice stronger than love :-)
+
+// The input will always be in lowercase and never be empty.
+
+
+function wordsToMarks(string){
+	let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+	let arr = string.split('').map(x => alphabet.indexOf(x))
+	return arr.reduce((acc, cur) => acc += cur, 0)
+}
+
+
+console.log(wordsToMarks("attitude"))		// 100
+console.log(wordsToMarks("friends"))		// 75
+console.log(wordsToMarks("family"))		// 66
+console.log(wordsToMarks("selfness"))		// 99
+console.log(wordsToMarks("knowledge"))		// 96
