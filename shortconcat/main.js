a28bc15926ab5f549ba9e438c9f7f5c85aaec4c3
Castle <castlerock88@protonmail.com>
2021-11-21 12:41:38 -0500

    solved

--- shortconcat/main.js
+++ shortconcat/main.js
@@ -7,7 +7,7 @@
 
 
 function shorter_reverse_longer(a,b){
-  return ""
+	return a.length >= b.length ? `${b}${a.split('').reverse().join('')}${b}` : `${a}${b.split('').reverse().join('')}${a}`
 }
 
 
