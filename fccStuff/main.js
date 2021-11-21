7544d0aea95ad26cb10da79c8e87bb438b3650be
Castle <castlerock88@protonmail.com>
2021-11-20 19:00:28 -0500

    tests passed

--- fccStuff/main.js
+++ fccStuff/main.js
@@ -139,27 +139,29 @@ function convertToRoman(num) {
 // console.log(convertToRoman(5))			// V.
 // console.log(convertToRoman(9))			// IX.
 
-console.log(convertToRoman(12))				// XII. - XII
-console.log(convertToRoman(16))				// XVI. - XVI
-console.log(convertToRoman(29))				// XXIX. - XXIX
-console.log(convertToRoman(44))				// XLIV. - XLIV
-console.log(convertToRoman(45))				// XLV.	 - XLV
-console.log(convertToRoman(68))				// LXVIII - LXVIII
-console.log(convertToRoman(83))				// LXXXIII - LXXXIII
-console.log(convertToRoman(97))				// XCVII - XCVII
-console.log(convertToRoman(99))				// XCIX -> XCIX
+
+// console.log(convertToRoman(12))				// XII. - XII
+// console.log(convertToRoman(16))				// XVI. - XVI
+// console.log(convertToRoman(29))				// XXIX. - XXIX
+// console.log(convertToRoman(44))				// XLIV. - XLIV
+// console.log(convertToRoman(45))				// XLV.	 - XLV
+// console.log(convertToRoman(68))				// LXVIII - LXVIII
+// console.log(convertToRoman(83))				// LXXXIII - LXXXIII
+// console.log(convertToRoman(97))				// XCVII - XCVII
+// console.log(convertToRoman(99))				// XCIX -> XCIX
 
 
-// console.log(convertToRoman(400))			// CD
-// console.log(convertToRoman(500))			// D
-// console.log(convertToRoman(501))			// DI
-// console.log(convertToRoman(649))			// DCXLIX
-// console.log(convertToRoman(798))			// DCCXCVIII
-// console.log(convertToRoman(891))			// DCCCXCI
+// console.log(convertToRoman(400))			// CD - CD
+// console.log(convertToRoman(500))			// D - D
+// console.log(convertToRoman(501))			// DI - DI
+// console.log(convertToRoman(649))			// DCXLIX - DCXLIX
+// console.log(convertToRoman(798))			// DCCXCVIII - DCCXCVIII
+// console.log(convertToRoman(891))			// DCCCXCI - DCCCXCI
 
-// console.log(convertToRoman(1000))			// M
-// console.log(convertToRoman(1004))			// MIV
-// console.log(convertToRoman(1006))			// MVI
-// console.log(convertToRoman(1023))			// MXXIII
-// console.log(convertToRoman(2014))			// MMXIV
-// console.log(convertToRoman(3999))			// MMMCMXCIX
+
+console.log(convertToRoman(1000))			// M
+console.log(convertToRoman(1004))			// MIV
+console.log(convertToRoman(1006))			// MVI
+console.log(convertToRoman(1023))			// MXXIII
+console.log(convertToRoman(2014))			// MMXIV
+console.log(convertToRoman(3999))			// MMMCMXCIX
