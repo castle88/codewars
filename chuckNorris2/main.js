76f5a6de31b355729f0ceb4514020b706c6e3551
Castle <castlerock88@protonmail.com>
2021-11-22 18:42:01 -0500

    works in console doesnt pass test? whatever

--- chuckNorris2/main.js
+++ chuckNorris2/main.js
@@ -17,7 +17,7 @@ function onePunch(items){return typeof items !== 'string' ? 'Broken!' : items.sp
 
 
 
-console.log(onePunch(4204))						// 'Broken!'
+console.log(onePunch(4321))						// 'Broken!'
 console.log(onePunch('Beard Knife Grenade Motorbike Hat'))		// 'Brd Grnd Ht Knif Motorbik'
 console.log(onePunch('Horse Rope Cups Car Beard'))			// 'Brd Cr Cups Hors Rop'
 console.log(onePunch('Friend Beer Beard Monkey Laptop'))		// 'Brd Br Frind Lptop Monky'
