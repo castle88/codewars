6dbb0cc050e23ddbdc095b21c0bd7b91aa0cff8f
Castle <castlerock88@protonmail.com>
2021-11-23 17:27:01 -0500

    testing

--- fccStuff/main.js
+++ fccStuff/main.js
@@ -496,5 +496,17 @@
 
 //Your solution should not use the Array.prototype.flat() or Array.prototype.flatMap() methods.
 
+// break apart binary string, convert binary numbers to decimal, then convert to characters
+
+function binaryAgent(str) {
+	return str.split(' ').map(x => String.fromCharCode(parseInt(x, 2))).join('')
+}
+
+console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"))
+// should return the string Aren't bonfires fun!?
+
+console.log(binaryAgent("01001001 00100000 01101100 01101111 01110110 01100101 00100000 01000110 01110010 01100101 01100101 01000011 01101111 01100100 01100101 01000011 01100001 01101101 01110000 00100001"))
+// should return the string I love FreeCodeCamp!
+
 
 // *************   TELEPHONE NUMBER VALIDATOR ********************
