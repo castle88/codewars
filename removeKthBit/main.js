2abb5fc38950d771a49d6af911eaccad6f2a050c
Castle <castlerock88@protonmail.com>
2021-11-25 14:47:11 -0500

    one works the rest dont?

--- removeKthBit/main.js
+++ removeKthBit/main.js
@@ -31,7 +31,18 @@
 
 
 function killKthBit(n, k) {
-  return //coding and coding..
+	const binary = n.toString(2).split('')
+	const switcher = (target)=> target === '1' ? '0' : '1'
+	const answer = binary.reduce((acc, cur, index) => {
+		if (index === k) {
+			cur = switcher(cur)
+		}
+		return acc += cur
+	},'')
+	console.log(binary.join(''))
+	console.log(answer)
+
+	return parseInt(answer, 2)
 }
 
 
@@ -39,6 +50,6 @@ function killKthBit(n, k) {
 console.log(killKthBit(37,3))				// 33
 console.log(killKthBit(37,4))				// 37
 console.log(killKthBit(0,4))				// 0
-console.log(killKthBit(2147483647,16))			// 2147450879
-console.log(killKthBit(374823748,13))			// 374819652
-console.log(killKthBit(1084197039,15))			// 1084197039
+//console.log(killKthBit(2147483647,16))			// 2147450879
+//console.log(killKthBit(374823748,13))			// 374819652
+//console.log(killKthBit(1084197039,15))			// 1084197039
