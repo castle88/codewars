956174b4bb3a95853a24f78a17066e0a4f5ac301
Castle <castlerock88@protonmail.com>
2021-11-27 00:38:12 -0500

    gatcha

--- freudianTranslator/main.js
+++ freudianTranslator/main.js
@@ -7,6 +7,7 @@
 // In this kata, the toFreud() function will take a string as its argument, and return a string with every word replaced by the explanation to everything, according to Freud. Note that an empty string, or no arguments, should result in the ouput being ""(empty string).
 
 function toFreud(string) {
+	if(string.length < 1) return ""
 	return string.split(' ').map(x => 'sex').join(' ')
 }
 
