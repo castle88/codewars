8a3aa9579ec9e095ca825f7e73b14f6f7467a937
Castle <castlerock88@protonmail.com>
2021-11-28 20:58:22 -0500

    sumofabeach init test

---
+++ sumOfABeach/main.js
@@ -0,0 +1,21 @@
+// Beaches are filled with sand, water, fish, and sun. Given a string, calculate how many times the words "Sand", "Water", "Fish", and "Sun" appear without overlapping (regardless of the case).
+// Examples
+
+// sumOfABeach("WAtErSlIde")                    ==>  1
+// sumOfABeach("GolDeNSanDyWateRyBeaChSuNN")    ==>  3
+// sumOfABeach("gOfIshsunesunFiSh")             ==>  4 
+// sumOfABeach("cItYTowNcARShoW")               ==>  0
+
+
+
+function sumOfABeach(beach) {
+	const regex = /(Water)|(Sand)|(Sun)|(Fish)/gmi
+	return beach.match(regex).length
+}
+
+
+console.log(sumOfABeach("SanD"))			// 1
+console.log(sumOfABeach("sunshine"))			// 1
+console.log(sumOfABeach("sunsunsunsun"))		// 4
+console.log(sumOfABeach("123FISH321"))			// 1
+console.log(sumOfABeach("GolDeNSanDyWateRyBeaChSuNN"))	// 3
