b945682a07cd1f4b6e60ebeafc24c59be7d27c9f
Castle <castlerock88@protonmail.com>
2021-11-29 19:49:15 -0500

    rearrange number setup

---
+++ rearrangeNumber/main.js
@@ -0,0 +1,20 @@
+// Create a function that takes one positive three digit integer and rearranges its digits to get the maximum possible number. Assume that the argument is an integer.
+
+// Return -1 if the argument is not valid
+
+// Return null (nil for Ruby, nothing for Julia) if the argument is not valid.
+
+// maxRedigit(123); // returns 321
+
+
+var maxRedigit = function(num) {
+  // Do yor magic :)
+};
+
+
+// Basic test
+console.log(maxRedigit(123))		// 321, "123 => 321");
+
+// Edge cases test
+console.log(maxRedigit(-1))		// null, "-1 => null");
+console.log(maxRedigit(0))		// null, "0 => null");
