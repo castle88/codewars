cce3e48037a265bbe91cbf831bbd81b69e899ee5
Castle <castlerock88@protonmail.com>
2021-12-02 06:16:13 -0500

    greater less than opperators backwards

--- safenUserInput/main.js
+++ safenUserInput/main.js
@@ -17,15 +17,15 @@
 
 
 function htmlspecialchars(formData) {
-	return formData.split('').map(x => x === '&' ? `&amp;` : x === '<' ? '&gt;' : x === '>' ? '&lt;' : x === `"` ? '&quot;' : x).join('')
+	return formData.split('').map(x => x === '&' ? `&amp;` : x === '<' ? '&lt;' : x === '>' ? '&gt;' : x === `"` ? '&quot;' : x).join('')
 }
 
 
 console.log(htmlspecialchars("<h2>Hello World</h2>"))
-//      "&lt;h2&gt;Hello World&lt;/h2&gt;"
+								//      "&lt;h2&gt;Hello World&lt;/h2&gt;"
 console.log(htmlspecialchars("Hello, how would you & I fare?"))
-//      "Hello, how would you &amp; I fare?"
+								//      "Hello, how would you &amp; I fare?"
 console.log(htmlspecialchars('How was "The Matrix"?  Did you like it?'))
-//      'How was &quot;The Matrix&quot;?  Did you like it?'
+								//      'How was &quot;The Matrix&quot;?  Did you like it?'
 console.log(htmlspecialchars("<script>alert('Website Hacked!');</script>"))
-//      "&lt;script&gt;alert('Website Hacked!');&lt;/script&gt;"
+								//      "&lt;script&gt;alert('Website Hacked!');&lt;/script&gt;"
