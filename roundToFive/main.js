4a3b416c2e1fd05c02c4857a37158fe43c064a49
Castle <castlerock88@protonmail.com>
2021-12-03 06:29:38 -0500

    this time setup

--- roundToFive/main.js
+++ roundToFive/main.js
@@ -21,15 +21,15 @@ function roundToNext5(n) {
   return n;
 }
 
-console.log(roundToNext5([0, 0]));
-console.log(roundToNext5([1, 5]));
-console.log(roundToNext5([-1, 0]));
-console.log(roundToNext5([-5, -5]));
-console.log(roundToNext5([3, 5]));
-console.log(roundToNext5([5, 5]));
-console.log(roundToNext5([7, 10]));
-console.log(roundToNext5([20, 20]));
-console.log(roundToNext5([39, 40]));
-console.log(roundToNext5([990, 990]));
-console.log(roundToNext5([121, 125]));
-console.log(roundToNext5([555, 555]));
+console.log(roundToNext5(0)); //  0
+console.log(roundToNext5(1)); //  5
+console.log(roundToNext5(-1)); //  0
+console.log(roundToNext5(-5)); // -5
+console.log(roundToNext5(3)); //  5
+console.log(roundToNext5(5)); //  5
+console.log(roundToNext5(7)); //  10
+console.log(roundToNext5(20)); //  20
+console.log(roundToNext5(39)); //  40
+console.log(roundToNext5(990)); //  990
+console.log(roundToNext5(121)); //  125
+console.log(roundToNext5(555)); //  555
