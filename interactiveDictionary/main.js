0554e3e3a8679a12d73b7ec0d5c617041c4a52f5
Castle <castlerock88@protonmail.com>
2021-12-04 04:36:17 -0500

    testing

--- interactiveDictionary/main.js
+++ interactiveDictionary/main.js
@@ -14,27 +14,29 @@
 
 class Dictionary {
   constructor() {
-    // your code
+    this.container = {};
   }
 
   newEntry(key, value) {
-    // your code
+    return (this.container[key] = value);
   }
 
   look(key) {
-    // your code
+    return this.container[key]
+      ? this.container[key]
+      : `Can't find entry for ${key}`;
   }
 }
 
 let d = new Dictionary();
 
 d.newEntry("Apple", "A fruit");
-console.log(d.look("Apple"), "A fruit");
+console.log(d.look("Apple")); // "A fruit"
 
 d.newEntry("Soccer", "A sport");
-console.log(d.look("Soccer"), "A sport");
-console.log(d.look("Hi"), "Can't find entry for Hi");
-console.log(d.look("Ball"), "Can't find entry for Ball");
+console.log(d.look("Soccer")); // "A sport"
+console.log(d.look("Hi")); // "Can't find entry for Hi"
+console.log(d.look("Ball")); // "Can't find entry for Ball"
 
 d.newEntry("soccer", "a sport");
-console.log(d.look("soccer"), "a sport");
+console.log(d.look("soccer")); // "a sport"
