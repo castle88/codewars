8bb26e3d68aa15e879b9e9fad51b3b2faffff85f
Castle <castlerock88@protonmail.com>
2021-12-04 05:13:33 -0500

    made that way more difficult than needed be

--- basicSubClasses/main.js
+++ basicSubClasses/main.js
@@ -3,11 +3,16 @@
 // You have to do God's job. The creation method must return an array of length 2 containing objects (representing Adam and Eve). The first object in the array should be an instance of the class Man. The second should be an instance of the class Woman. Both objects have to be subclasses of Human. Your job is to implement the Human, Man and Woman classes.
 
 class God {
-  static create() {}
+  static create() {
+    const adam = new Man("Adam");
+    const eve = new Woman("Eve");
+    return [adam, eve];
+  }
 }
 
 class Human extends God {
   constructor(name) {
+    super();
     this.name = name;
   }
 }
