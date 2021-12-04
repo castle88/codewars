f26a6d7458e495e3f82c463090cb61adedfaa795
Castle <castlerock88@protonmail.com>
2021-12-04 03:52:07 -0500

    setup

---
+++ enumerableMagic/main.js
@@ -0,0 +1,9 @@
+// Create a method take that accepts a list/array and a number n, and returns a list/array array of the first n elements from the list/array.
+
+// If you need help, here's a reference:
+
+function take(arr, n) {
+  // Your code here
+}
+
+console.log(take([0, 1, 2, 3, 5, 8, 13], 3)); // [0, 1, 2], "should return the first 3 items"
