b9e01511f6b877d20efffd361e92741204e8170d
Castle <castlerock88@protonmail.com>
2021-12-04 04:22:47 -0500

    solved

--- methodForCounting/main.js
+++ methodForCounting/main.js
@@ -23,7 +23,7 @@ function List() {
         .join("")
         .split("")
         .filter((x) => Number(x) >= 0);
-      console.log(singleDigits);
+
       const count = singleDigits.reduce((obj, el) => {
         obj[el] ? obj[el]++ : (obj[el] = 1);
         return obj;
@@ -40,20 +40,20 @@ var l = new List();
 
 let integersList = [1, 1, 2, 3, 1, 2, 3, 4];
 let digitsList = [1, 3];
-// //console.log(l.countSpecDigits(integersList, digitsList)); //[[1, 3], [3, 2]]);
+console.log(l.countSpecDigits(integersList, digitsList)); //[[1, 3], [3, 2]]);
 
 integersList = [-18, -31, 81, -19, 111, -888];
 digitsList = [1, 8, 4];
 console.log(l.countSpecDigits(integersList, digitsList)); // [[1, 7], [8, 5], [4, 0]]);
 
-// integersList = [-77, -65, 56, -79, 6666, 222];
-// digitsList = [1, 8, 4];
-// //console.log(l.countSpecDigits(integersList, digitsList)); //  [[1, 0], [8, 0], [4, 0]]);
+integersList = [-77, -65, 56, -79, 6666, 222];
+digitsList = [1, 8, 4];
+console.log(l.countSpecDigits(integersList, digitsList)); //  [[1, 0], [8, 0], [4, 0]]);
 
-// integersList = [-77, -65, 56, -79, 6666, 222];
-// digitsList = [];
-// //console.log(l.countSpecDigits(integersList, digitsList)); // []);
+integersList = [-77, -65, 56, -79, 6666, 222];
+digitsList = [];
+console.log(l.countSpecDigits(integersList, digitsList)); // []);
 
-// integersList = [];
-// digitsList = [1, 8, 4];
-// //console.log(l.countSpecDigits(integersList, digitsList)); // [[1, 0], [8, 0], [4, 0]]);
+integersList = [];
+digitsList = [1, 8, 4];
+console.log(l.countSpecDigits(integersList, digitsList)); // [[1, 0], [8, 0], [4, 0]]);
