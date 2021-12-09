68ee2ddeff0f5e8741001f520659293010e1a10e
Castle <castlerock88@protonmail.com>
2021-12-09 18:21:52 -0500

    testing

--- reversedWords/main.js
+++ reversedWords/main.js
@@ -5,7 +5,7 @@
 // "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
 
 function reverseWords(str) {
-  return str; // reverse those words
+  return str.split(" ").reverse().join(" ");
 }
 
 console.log(reverseWords("hello world!")); // "world! hello"
