3b3189d390257daad6098fb26d1b2e382cbb9d56
Castle <castlerock88@protonmail.com>
2021-12-09 18:20:26 -0500

    setup for real

--- reversedWords/main.js
+++ reversedWords/main.js
@@ -8,16 +8,8 @@ function reverseWords(str) {
   return str; // reverse those words
 }
 
-reverseWords("hello world!")(
-  // "world! hello"
-  reverseWords("yoda doesn't speak like this")
-)(
-  //  "this like speak doesn't yoda"
-  reverseWords("foobar")
-)(
-  //  "foobar"
-  reverseWords("kata editor")
-)(
-  //  "editor kata"
-  reverseWords("row row row your boat")
-); //  "boat your row row row"
+console.log(reverseWords("hello world!")); // "world! hello"
+console.log(reverseWords("yoda doesn't speak like this")); //  "this like speak doesn't yoda"
+console.log(reverseWords("foobar")); //  "foobar"
+console.log(reverseWords("kata editor")); //  "editor kata"
+console.log(reverseWords("row row row your boat")); //  "boat your row row row"
