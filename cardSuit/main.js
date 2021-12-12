c154a81dcccf7a51851c1d1402e382ce8fd1bc6a
Castle <castlerock88@protonmail.com>
2021-12-12 16:37:14 -0500

    setup

---
+++ cardSuit/main.js
@@ -0,0 +1,26 @@
+// You get any card as an argument. Your task is to return a suit of this card.
+
+// Our deck (is preloaded):
+
+// deck = ['2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣','A♣',
+//         '2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦','A♦',
+//         '2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♥',
+//         '2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠','A♠'];
+
+// ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
+
+// Fundamentals
+// Strings
+// Basic Language Featur
+
+function defineSuit(card) {
+  // good luck
+}
+
+console.log(defineSuit("3♣")); // 'clubs'
+console.log(defineSuit("Q♠")); // 'spades'
+console.log(defineSuit("9♦")); // 'diamonds'
+console.log(defineSuit("J♥")); // 'hearts'
