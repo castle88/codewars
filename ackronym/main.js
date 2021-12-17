7468548dcfb095002306a631b645047b7a625ba6
Castle <castlerock88@protonmail.com>
2021-12-16 19:44:07 -0500

    setup ackronym

---
+++ ackronym/main.js
@@ -0,0 +1,28 @@
+// Implement a function called makeAcronym that returns the first letters of each word in a passed in string.
+
+// Make sure the letters returned are uppercase.
+
+// If the value passed in is not a string return 'Not a string'.
+
+// If the value passed in is a string which contains characters other than spaces and alphabet letters, return 'Not letters'.
+
+// If the string is empty, just return the string itself: "".
+
+// EXAMPLES:
+
+// 'Hello codewarrior' -> 'HC'
+
+// 'a42' -> 'Not letters'
+
+// 42 -> 'Not a string'
+
+// [2,12] -> 'Not a string'
+
+// {name: 'Abraham'} -> 'Not a string'
+
+var makeAcronym = function (string) {};
+
+console.log(makeAcronym("My aunt sally")); // 'MAS'
+console.log(makeAcronym("42")); // 'Not letters'
+console.log(makeAcronym(64)); // 'Not a string'
+console.log(makeAcronym([2, 4, 6, 8])); // 'Not a string'
