32b65ce148499d07e485b3b4471b7ece43751665
Castle <castlerock88@protonmail.com>
2021-12-26 20:15:16 -0500

    setup

---
+++ alternating/main.js
@@ -0,0 +1,22 @@
+// Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+
+// For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+// The input will be a lowercase string with no spaces.
+
+// Good luck!
+
+// If you like this Kata, please try:
+
+// Indexed capitalization
+
+// Even-odd disparity
+
+function capitalize(s) {
+  return [];
+}
+
+console.log(capitalize("abcdef")); // ['AbCdEf', 'aBcDeF']);
+console.log(capitalize("codewars")); // ['CoDeWaRs', 'cOdEwArS']);
+console.log(capitalize("abracadabra")); // ['AbRaCaDaBrA', 'aBrAcAdAbRa']);
+console.log(capitalize("codewarriors")); // ['CoDeWaRrIoRs', 'cOdEwArRiOrS']);
