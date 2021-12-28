9f72c93bb5d089bccf9dfd621046fc569463af75
Castle <castlerock88@protonmail.com>
2021-12-28 03:44:28 -0500

    testing solutions

--- alphabetacalGrid/main.js
+++ alphabetacalGrid/main.js
@@ -36,24 +36,88 @@ function grid(N) {
 
   const container = [];
   for (let i = 0; i < N; i++) {
-    if (alphabet.slice(i, N + i).length !== N) {
-      let end = alphabet.slice(i, N + i);
-      let overFlow = alphabet.slice(0, N - end.length);
-      container.push(end.concat(overFlow));
-    } else {
-      container.push(alphabet.slice(i, N + i));
-    }
+    let end = alphabet.slice(i, N + i);
+    console.log(end);
+
+    let overFlow = alphabet.slice(0, N - end.length);
+    console.log(overFlow);
+
+    container.push(end.concat(overFlow));
   }
   const answer = container.map((x) => x.join(" "));
 
   return answer.join("\n");
 }
 
-console.log(grid(0)); // ''
-console.log(grid(1)); // 'a'
-console.log(grid(2)); // 'a b\nb c'
-console.log(grid(4)); // 'a b c d\nb c d e\nc d e f\nd e f g'
-console.log(grid(6)); // 'a b c d e f\nb c d e f g\nc d e f g h\nd e f g h i\ne f g h i j\nf g h i j k'
+// console.log(grid(0)); // ''
+// console.log(grid(1)); // 'a'
+// console.log(grid(2)); // 'a b\nb c'
+// console.log(grid(4)); // 'a b c d\nb c d e\nc d e f\nd e f g'
+// console.log(grid(6)); // 'a b c d e f\nb c d e f g\nc d e f g h\nd e f g h i\ne f g h i j\nf g h i j k'
 
-console.log(grid(-1)); // null
-console.log(grid(-5)); // null
+// console.log(grid(-1)); // null
+// console.log(grid(-5)); // null
+
+console.log(grid(27));
+
+// output
+/*
+  'a b c d e f g h i j k l m n o p q r s t u v w x y z a
+ \nb c d e f g h i j k l m n o p q r s t u v w x y z a b
+ \nc d e f g h i j k l m n o p q r s t u v w x y z a b c
+ \nd e f g h i j k l m n o p q r s t u v w x y z a b c d
+ \ne f g h i j k l m n o p q r s t u v w x y z a b c d e
+ \nf g h i j k l m n o p q r s t u v w x y z a b c d e f
+ \ng h i j k l m n o p q r s t u v w x y z a b c d e f g
+ \nh i j k l m n o p q r s t u v w x y z a b c d e f g h
+ \ni j k l m n o p q r s t u v w x y z a b c d e f g h i
+ \nj k l m n o p q r s t u v w x y z a b c d e f g h i j
+ \nk l m n o p q r s t u v w x y z a b c d e f g h i j k
+ \nl m n o p q r s t u v w x y z a b c d e f g h i j k l
+ \nm n o p q r s t u v w x y z a b c d e f g h i j k l m
+ \nn o p q r s t u v w x y z a b c d e f g h i j k l m n
+ \no p q r s t u v w x y z a b c d e f g h i j k l m n o
+ \np q r s t u v w x y z a b c d e f g h i j k l m n o p
+ \nq r s t u v w x y z a b c d e f g h i j k l m n o p q
+ \nr s t u v w x y z a b c d e f g h i j k l m n o p q r
+ \ns t u v w x y z a b c d e f g h i j k l m n o p q r s
+ \nt u v w x y z a b c d e f g h i j k l m n o p q r s t
+ \nu v w x y z a b c d e f g h i j k l m n o p q r s t u
+ \nv w x y z a b c d e f g h i j k l m n o p q r s t u v
+ \nw x y z a b c d e f g h i j k l m n o p q r s t u v w
+ \nx y z a b c d e f g h i j k l m n o p q r s t u v w x
+ \ny z a b c d e f g h i j k l m n o p q r s t u v w x y
+ \nz a b c d e f g h i j k l m n o p q r s t u v w x y z
+ \na b c d e f g h i j k l m n o p q r s t u v w x y z' 
+*/
+
+// expected solution
+/* 
+'a b c d e f g h i j k l m n o p q r s t u v w x y z a
+\nb c d e f g h i j k l m n o p q r s t u v w x y z a b
+\nc d e f g h i j k l m n o p q r s t u v w x y z a b c
+\nd e f g h i j k l m n o p q r s t u v w x y z a b c d
+\ne f g h i j k l m n o p q r s t u v w x y z a b c d e
+\nf g h i j k l m n o p q r s t u v w x y z a b c d e f
+\ng h i j k l m n o p q r s t u v w x y z a b c d e f g
+\nh i j k l m n o p q r s t u v w x y z a b c d e f g h
+\ni j k l m n o p q r s t u v w x y z a b c d e f g h i
+\nj k l m n o p q r s t u v w x y z a b c d e f g h i j
+\nk l m n o p q r s t u v w x y z a b c d e f g h i j k
+\nl m n o p q r s t u v w x y z a b c d e f g h i j k l
+\nm n o p q r s t u v w x y z a b c d e f g h i j k l m
+\nn o p q r s t u v w x y z a b c d e f g h i j k l m n
+\no p q r s t u v w x y z a b c d e f g h i j k l m n o
+\np q r s t u v w x y z a b c d e f g h i j k l m n o p
+\nq r s t u v w x y z a b c d e f g h i j k l m n o p q
+\nr s t u v w x y z a b c d e f g h i j k l m n o p q r
+\ns t u v w x y z a b c d e f g h i j k l m n o p q r s
+\nt u v w x y z a b c d e f g h i j k l m n o p q r s t
+\nu v w x y z a b c d e f g h i j k l m n o p q r s t u
+\nv w x y z a b c d e f g h i j k l m n o p q r s t u v
+\nw x y z a b c d e f g h i j k l m n o p q r s t u v w
+\nx y z a b c d e f g h i j k l m n o p q r s t u v w x
+\ny z a b c d e f g h i j k l m n o p q r s t u v w x y
+\nz a b c d e f g h i j k l m n o p q r s t u v w x y z
+\na b c d e f g h i j k l m n o p q r s t u v w x y z a'
+*/
