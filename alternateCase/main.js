d615f0a978ea97711104ca10c7f62eb7dc5ff689
Castle <castlerock88@protonmail.com>
2021-12-27 22:34:13 -0500

    setup

---
+++ alternateCase/main.js
@@ -0,0 +1,9 @@
+// Write function alternateCase which switch every letter in string from upper to lower and from lower to upper. E.g: Hello World -> hELLO wORLD
+
+function alternateCase(s) {
+  return "";
+}
+
+console.log(alternateCase("abc")); // "ABC"
+console.log(alternateCase("ABC")); // "abc"
+console.log(alternateCase("Hello World")); // "hELLO wORLD"
