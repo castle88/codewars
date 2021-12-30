52a14304bd8f758d0e774691410fc23d95759fd2
Castle <castlerock88@protonmail.com>
2021-12-30 00:13:00 -0500

    testing

--- finalGrade/main.js
+++ finalGrade/main.js
@@ -25,7 +25,15 @@
 // *Use Comparison and Logical Operators.
 
 function finalGrade(exam, projects) {
-  return; // final grade
+  if (exam > 90 || projects > 10) {
+    return 100;
+  } else if (exam > 75 && projects >= 5) {
+    return 90;
+  } else if (exam > 50 && projects >= 2) {
+    return 75;
+  } else {
+    return 0;
+  }
 }
 
 console.log(finalGrade(100, 12)); //  100
