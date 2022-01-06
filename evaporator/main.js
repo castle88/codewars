9f68689dfb688e7d83ceb17d676aa3b3f698fb32
Castle <castlerock88@protonmail.com>
2022-01-06 10:23:32 -0500

    poor logic, im not sure

--- evaporator/main.js
+++ evaporator/main.js
@@ -12,7 +12,13 @@
 // Content is in fact not necessary in the body of the function "evaporator", you can use it or not use it, as you wish. Some people might prefer to reason with content, some other with percentages only. It's up to you but you must keep it as a parameter because the tests have it as an argument.
 
 function evaporator(content, evap_per_day, threshold) {
-  return -1;
+  let daysTilBusted = 0;
+  while (content > content * (threshold * 0.01)) {
+    daysTilBusted++;
+    content -= evap_per_day * 0.01;
+    console.log(content);
+  }
+  return daysTilBusted;
 }
 
 console.log(evaporator(10, 10, 10)); // 22
