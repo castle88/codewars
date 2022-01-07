625622064d488bcab08c87de55ed9a7d24f6e57e
Castle <castlerock88@protonmail.com>
2022-01-07 06:07:50 -0500

    testing

--- rpsls/main.js
+++ rpsls/main.js
@@ -18,7 +18,23 @@
 // Values will be given as one of "rock", "spock", "paper", "lizard", "scissors".
 
 function rpsls(pl1, pl2) {
-  //Your Magnificent Code here
+  if (
+    (pl1 === "scissors" && pl2 === "paper") ||
+    (pl1 === "paper" && pl2 === "rock") ||
+    (pl1 === "rock" && pl2 === "lizard") ||
+    (pl1 === "lizard" && pl2 === "spock") ||
+    (pl1 === "spock" && pl2 === "scissors") ||
+    (pl1 === "scissors" && pl2 === "lizard") ||
+    (pl1 === "lizard" && pl2 === "paper") ||
+    (pl1 === "paper" && pl2 === "spock") ||
+    (pl1 === "spock" && pl2 === "rock") ||
+    (pl1 === "rock" && pl2 === "scissors")
+  )
+    return "Player 1 Won!";
+
+  if (pl1 === pl2) return "Draw!";
+
+  return "Player 2 Won!";
 }
 
 // pl1 win
