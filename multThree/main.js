2b5c9dc9bc2c797507954be79c32d3157989d135
Castle <castlerock88@protonmail.com>
2022-01-08 10:17:46 -0500

    setup

---
+++ multThree/main.js
@@ -0,0 +1,14 @@
+// Given a positive integer n: 0 < n < 1e6, remove the last digit until you're left with a number that is a multiple of three.
+
+// Return n if the input is already a multiple of three, and return -1 if no such number exists.
+// Examples
+
+// 1      => -1
+// 25     => -1
+// 36     => 36
+// 1244   => 12
+// 952406 => 9
+
+const prevMultOfThree = (n) => {};
+
+console.log(prevMultOfThree(1244)); // 12
