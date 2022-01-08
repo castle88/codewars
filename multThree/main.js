1473202f1cf29a68bea640d983a6ac816b7f381f
Castle <castlerock88@protonmail.com>
2022-01-08 10:32:42 -0500

    testing

--- multThree/main.js
+++ multThree/main.js
@@ -9,6 +9,28 @@
 // 1244   => 12
 // 952406 => 9
 
-const prevMultOfThree = (n) => {};
+const prevMultOfThree = (n) => {
+  if (n % 3 === 0) {
+    return n;
+  } else {
+    const charArr = String(n).split("");
+    console.log(charArr);
+    if (charArr.length > 1) {
+      const last = charArr.pop();
+      const removeLast = Number(charArr.join(""));
 
+      console.log(last);
+      console.log(charArr);
+      console.log(removeLast);
+
+      return prevMultOfThree(removeLast);
+    } else {
+      return -1;
+    }
+  }
+};
+
+console.log(prevMultOfThree(36)); // 36
 console.log(prevMultOfThree(1244)); // 12
+console.log(prevMultOfThree(952406)); // 9
+console.log(prevMultOfThree(25)); // -1
