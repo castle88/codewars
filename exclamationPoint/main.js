8ef55f666eb65b18d51a772074d9a3d8238eeb70
Castle <castlerock88@protonmail.com>
2022-01-13 11:48:43 -0500

    happy to see i still dont understand regex

--- exclamationPoint/main.js
+++ exclamationPoint/main.js
@@ -11,16 +11,18 @@
 // remove("Hi") === "Hi"
 
 function remove(string) {
-  console.log(string);
-  const regex = /!+$/g;
-  const matchLength =
-    string.match(regex) === null ? 0 : string.match(regex)[0].length;
-  const newString = string
-    .split("")
-    .filter((x, i) => i < string.length - matchLength)
-    .join("");
+  // ********* did all this because global flag in regex REMEMBER THIS ******
 
-  return newString;
+  //   console.log(string);
+  //   const regex = /!+$/g;
+  //   const matchLength =
+  //     string.match(regex) === null ? 0 : string.match(regex)[0].length;
+  //   const newString = string
+  //     .split("")
+  //     .filter((x, i) => i < string.length - matchLength)
+  //     .join("");
+  return string.replace(/!+$/, "");
+  //   return newString;
 }
 
 console.log(remove("Hi!")); // "Hi"
