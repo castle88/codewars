d0184638be9441e7585223e8fb2a3137ed47f80d
Castle <castlerock88@protonmail.com>
2022-01-13 11:45:57 -0500

    not elegant but works

--- exclamationPoint/main.js
+++ exclamationPoint/main.js
@@ -11,12 +11,21 @@
 // remove("Hi") === "Hi"
 
 function remove(string) {
-  return "";
+  console.log(string);
+  const regex = /!+$/g;
+  const matchLength =
+    string.match(regex) === null ? 0 : string.match(regex)[0].length;
+  const newString = string
+    .split("")
+    .filter((x, i) => i < string.length - matchLength)
+    .join("");
+
+  return newString;
 }
 
 console.log(remove("Hi!")); // "Hi"
 console.log(remove("Hi!!!")); // "Hi"
-console.log(remove("Hi")); // "!Hi"
-console.log(remove("Hi!")); // "!Hi"
-console.log(remove("Hi Hi!")); // "Hi! Hi"
+console.log(remove("!Hi")); // "!Hi"
+console.log(remove("!Hi!")); // "!Hi"
+console.log(remove("Hi! Hi!")); // "Hi! Hi"
 console.log(remove("Hi")); // "Hi"
