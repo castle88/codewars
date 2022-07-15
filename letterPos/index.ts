e54480e254ffcef74d0ad9f779c0f9c76c4a0cca
Castle <castlerock88@protonmail.com>
2022-07-15 14:54:55 -0400

    alphabet position

---
+++ letterPos/index.ts
@@ -0,0 +1,14 @@
+// When provided with a letter, return its position in the alphabet.
+
+// Input :: "a"
+
+// Ouput :: "Position of alphabet: 1"
+
+function position(letter: string): string {
+  const alphabet: string = "abcdefghijklmnopqrstuvwxyz";
+  return `Position of alphabet: ${alphabet.indexOf(letter) + 1}`;
+}
+
+console.log(position("a")); // "Position of alphabet: 1"
+console.log(position("z")); // "Position of alphabet: 26"
+console.log(position("e")); // "Position of alphabet: 5"
