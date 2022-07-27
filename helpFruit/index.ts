571437ecf22231cdf66eb41c52c41ffd40a54481
Castle <castlerock88@protonmail.com>
2022-07-27 03:04:42 -0400

    fruit guy

---
+++ helpFruit/index.ts
@@ -0,0 +1,18 @@
+// Our fruit guy has a bag of fruit (represented as an array of strings) where some fruits are rotten. He wants to replace all the rotten pieces of fruit with fresh ones. For example, given ["apple","rottenBanana","apple"] the replaced array should be ["apple","banana","apple"]. Your task is to implement a method that accepts an array of strings containing fruits should returns an array of strings where all the rotten fruits are replaced by good ones.
+// Notes
+
+//     If the array is null/nil/None or empty you should return empty array ([]).
+//     The rotten fruit name will be in this camelcase (rottenFruit).
+//     The returned array should be in lowercase.
+
+function removeRotten(bagOfFruits: string[]) {
+  if (!bagOfFruits[0]) return bagOfFruits;
+
+  const fresh = bagOfFruits.map((fruit) =>
+    fruit.match(/rotten/) ? fruit.replace("rotten", "").toLowerCase() : fruit
+  );
+  return fresh;
+}
+
+console.log(removeRotten(["apple", "banana", "kiwi", "melone", "orange"])); // ["apple","banana","kiwi","melone","orange"]
+console.log(removeRotten([])); // []
