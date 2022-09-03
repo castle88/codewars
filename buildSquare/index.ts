0970c41c35ef3fa87400590a28bc806f1eaa7a71
Castle <castlerock88@protonmail.com>
2022-09-03 18:45:04 -0400

    binary searches

--- buildSquare/index.ts
+++ buildSquare/index.ts
@@ -1,11 +1,11 @@
-// I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 1 and 50.
-// Example
+I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 1 and 50.
+Example
 
-// n = 3, so I expect a 3x3 square back just like below as a string:
+n = 3, so I expect a 3x3 square back just like below as a string:
 
-// +++
-// +++
-// +++
++++
++++
++++
 
 function generateShape(integer: number): string {
   let square = "";
@@ -20,5 +20,3 @@ function generateShape(integer: number): string {
   }
   return square;
 }
-
-console.log(generateShape(8)); // '++++++++\n++++++++\n++++++++\n++++++++\n++++++++\n++++++++\n++++++++\n++++++++'
